0390d6f44dd862d43757c6b8cd4adf2db474e146
Thomas Ramiro <116609321+ThomRamks@users.noreply.github.com>
2023-01-25 09:22:24 -0300

    Incorporando interface

--- Banco/src/Application.java
+++ Banco/src/Application.java
@@ -1,5 +1,5 @@
 import banco.Banco;
-
+import cliente.Cliente;
 import util.GeraDadosIniciais;
 
 import java.util.Scanner;
@@ -16,6 +16,7 @@ public class Application {
         GeraDadosIniciais dadosIniciais = new GeraDadosIniciais();
         dadosIniciais.carregaDadosIniciais();
         Banco banco = Banco.getInstance();
+    }
 
 //        String documentoArthur = "578.179.380-16";
 //        banco.listarContasUsuario(documentoArthur);
@@ -79,14 +80,68 @@ public class Application {
 //        operacao.sacar(ccPessoaJuridica, 200);
 //        System.out.printf("Saldo Conta Corrente PJ: %.2f%n",ccPessoaJuridica.getSaldo());
 
+
+    public void menuInicial() {
+        System.out.println("====================================================");
+        System.out.println("             Seja bem vindo(a) ao                   ");
+        System.out.println("		            ADA BANK                        ");
+        System.out.println("          Seu dinheiro, nossa renda!                ");
+        System.out.println("====================================================");
+        System.out.println("");
+        System.out.println("Digite a operação desejada:\n"
+                + "1 - Acessar sua conta \n"
+                + "2 - Abrir conta \n"
+                + "3 - Sair");
+
+        respostasUsuario = sc.next();
+
+        switch (respostasUsuario) {
+            case "1":
+                //fazerLogin();
+                break;
+            case "2":
+                abrirConta();
+                break;
+            case "3":
+                //sair();
+                break;
+            default:
+                System.out.println("Operação inválida. Tente novamente.");
+                menuInicial();
+                break;
+        }
+
+
+        //validarRequisicao(respostasUsuario);
     }
 
-   /* public void menuUsuario() {
-        System.out.println("Seja Bem-Vindo ao AdaBank! Acesse sua conta ou abra uma!\n Acessar sua conta (1)\n Abertura de Conta (2)");
+    private void abrirConta() {
+        System.out.println("Qual tipo de conta voce deseja criar:\n"
+                + "1 - Conta Pessoa Fisica \n"
+                + "2 - Conta Pessoa Juridica \n"
+                + "3 - Voltar ao Menu Inicial");
+
         respostasUsuario = sc.next();
-        validarRequisicao(respostasUsuario);
+
+        switch (respostasUsuario) {
+            case "1":
+                //abrircontaPF();
+                break;
+            case "2":
+                //abrirContaPJ();
+                break;
+            case "3":
+                menuInicial();
+                break;
+            default:
+                System.out.println("Operação inválida. Tente novamente.");
+                abrirConta();
+                break;
+        }
     }
-    public void validarRequisicao(String requisicaoDoUsuario) {
+
+
+    /*public void validarRequisicao(String requisicaoDoUsuario) {
         if (!requisicaoDoUsuario.equals("1") & !requisicaoDoUsuario.equals("2")) {
             System.out.println("Não entendemos sua requisição, tente novamente!");
             menuUsuario();
