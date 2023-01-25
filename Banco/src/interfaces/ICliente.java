d2f6685e877de811f3531f310c32c7917d0f51d1
Thomas Ramiro <116609321+ThomRamks@users.noreply.github.com>
2023-01-25 15:13:03 -0300

    Update ICliente.java

--- Banco/src/interfaces/ICliente.java
+++ Banco/src/interfaces/ICliente.java
@@ -1,8 +1,11 @@
 package interfaces;
 
+import java.util.List;
+
 public interface ICliente {
     String getNome();
     String getDocumento();
+    List<IConta> getContasUsuario();
     boolean validaSenha(String senha);
 
 }
