ed84a3c8207f06ce434aabb88cf5256aa2cc77f1
Diego <diego.ruescas@gmail.com>
2023-01-25 17:17:25 -0300

    Adição pacote valida - ValidaTexo ValidaDocumento

--- Banco/src/Application.java
+++ Banco/src/Application.java
@@ -1,6 +1,8 @@
 import banco.Banco;
 import cliente.Cliente;
 import util.GeraDadosIniciais;
+import util.valida.ValidaDocumento;
+import util.valida.ValidaTexto;
 
 import java.util.Scanner;
 
@@ -16,6 +18,43 @@ public class Application {
         GeraDadosIniciais dadosIniciais = new GeraDadosIniciais();
         dadosIniciais.carregaDadosIniciais();
         Banco banco = Banco.getInstance();
+
+        String cpf = "578.179.380-16";
+        System.out.printf("CPF %s válido? %b%n", cpf, ValidaDocumento.isCpf(cpf));
+
+        String cnpj = "24.861.255/0001-07";
+        System.out.printf("CNPJ %s válido? %b%n", cnpj, ValidaDocumento.isCNPJ(cnpj));
+
+        cnpj = "24861255000107";
+        System.out.printf("CNPJ %s válido? %b%n", cnpj, ValidaDocumento.isCNPJ(cnpj));
+
+        cnpj = "24861255/0001-07";
+        System.out.printf("CNPJ %s válido? %b%n", cnpj, ValidaDocumento.isCNPJ(cnpj));
+
+        cnpj = "01.234.567/0001-11";
+        System.out.printf("CNPJ %s válido? %b%n", cnpj, ValidaDocumento.isCNPJ(cnpj));
+
+        String nome = "Di3go Ruescas";
+        System.out.printf("Nome %s é válido? %b%n", nome, ValidaTexto.somenteLetras(nome));
+
+        nome = "Diego Ruescas";
+        System.out.printf("Nome %s é válido? %b%n", nome, ValidaTexto.somenteLetras(nome));
+
+        nome = "dIeGo rUesCAS";
+        System.out.printf("Nome %s = %s%n", nome, ValidaTexto.upperfirstCase(nome));
+
+        nome = "Ada Tecnologia e Educação S.A.";
+        System.out.printf("Nome %s = %s%n", nome, ValidaTexto.upperfirstCase(nome));
+
+        nome = "aDaBAnK ltDa";
+        System.out.printf("Nome %s = %s%n", nome, ValidaTexto.upperfirstCase(nome));
+
+        String senha = "12 34 56";
+        System.out.printf("Senha %s tem espaço? %b%n", senha, ValidaTexto.temEspaco(senha));
+
+        senha = "123456";
+        System.out.printf("Senha %s tem espaço? %b%n", senha, ValidaTexto.temEspaco(senha));
+
     }
 
 //        String documentoArthur = "578.179.380-16";
